2eda1a3c5cc2ec6ada8bfe7f6c8b6f976de948be
Fang Chung Wu <fangchung_wu@g-mail.nsysu.edu.tw>
2022-03-07 15:46:38 +0800

    update evaluation way

--- engine.py
+++ engine.py
@@ -1,4 +1,6 @@
 import math
+import time
+import numpy as np
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, scaler=None):
     model.train()
@@ -49,3 +51,137 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
     return metric_logger
+
+def evaluate_compute_batch_ap(model, data_loader_test, device='cpu'):
+    cpu_device = torch.device("cpu")
+    model.eval()
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = "Test:"
+    APs = []
+    for images, targets in metric_logger.log_every(data_loader_test, 100, header):
+        model_time = time.time()
+        prediction = model(image)
+        
+        #parameters
+        gt_boxes = target[0]['boxes']
+        gt_class_ids = target[0]['labels']
+        pred_boxes = prediction[0]['boxes']
+        pred_scores = prediction[0]['scores']
+        pred_class_ids = prediction[0]['labels']
+        model_time = time.time() - model_time
+        #Computer AP
+        evaluator_time = time.time()
+        AP, precisions, recalls, overlaps = compute_ap(gt_boxes, gt_class_ids, 
+                                               pred_boxes,pred_class_ids, pred_scores)
+        APs.append(AP)
+        metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
+    return APs
+
+def compute_overlaps(boxes1, boxes2):
+    """Computes IoU overlaps between two sets of boxes.
+    boxes1, boxes2: [N, (y1, x1, y2, x2)].
+    For better performance, pass the largest set first and the smaller second.
+    """
+    # Areas of anchors and GT boxes
+    area1 = (boxes1[:, 2] - boxes1[:, 0]) * (boxes1[:, 3] - boxes1[:, 1])
+    area2 = (boxes2[:, 2] - boxes2[:, 0]) * (boxes2[:, 3] - boxes2[:, 1])
+
+    # Compute overlaps to generate matrix [boxes1 count, boxes2 count]
+    # Each cell contains the IoU value.
+    overlaps = np.zeros((boxes1.shape[0], boxes2.shape[0]))
+    for i in range(overlaps.shape[1]):
+        box2 = boxes2[i]
+        overlaps[:, i] = compute_iou(box2, boxes1, area2[i], area1)
+    return overlaps
+
+def compute_iou(box, boxes, box_area, boxes_area):
+    
+    """Calculates IoU of the given box with the array of the given boxes.
+    box: 1D vector [y1, x1, y2, x2]
+    boxes: [boxes_count, (y1, x1, y2, x2)]
+    box_area: float. the area of 'box'
+    boxes_area: array of length boxes_count.
+    Note: the areas are passed in rather than calculated here for
+          efficency. Calculate once in the caller to avoid duplicate work.
+    """
+    # Calculate intersection areas
+
+    y1 = np.maximum(box[0], boxes[:, 0])
+    y2 = np.minimum(box[2], boxes[:, 2])
+    x1 = np.maximum(box[1], boxes[:, 1])
+    x2 = np.minimum(box[3], boxes[:, 3])
+    intersection = np.maximum(x2 - x1, 0) * np.maximum(y2 - y1, 0)
+    union = box_area + boxes_area[:] - intersection[:]
+    iou = intersection / union
+    return iou
+
+def compute_ap(gt_boxes, gt_class_ids, 
+               pred_boxes, pred_class_ids, pred_scores, 
+               iou_threshold=0.5):
+    """Compute Average Precision at a set IoU threshold (default 0.5).
+    Returns:
+    mAP: Mean Average Precision
+    precisions: List of precisions at different class score thresholds.
+    recalls: List of recall values at different class score thresholds.
+    overlaps: [pred_boxes, gt_boxes] IoU overlaps.
+    """
+    # Trim zero padding and sort predictions by score from high to low
+    # TODO: cleaner to do zero unpadding upstream
+    gt_boxes       = gt_boxes.detach().numpy()
+    gt_class_ids   = gt_class_ids.detach().numpy()
+    pred_boxes     = pred_boxes.detach().numpy()
+    pred_class_ids = pred_class_ids.detach().numpy()
+    pred_scores    = pred_scores.detach().numpy()
+    
+    indices        = np.argsort(pred_scores)[::-1]
+    pred_boxes     = pred_boxes[indices]
+    pred_scores    = pred_scores[indices]
+    pred_boxes     = pred_boxes[indices]
+    pred_class_ids = pred_class_ids[indices]
+
+
+    # Compute IoU overlaps [pred_masks, gt_masks]
+    overlaps = compute_overlaps(pred_boxes, gt_boxes)
+
+    # Loop through ground truth boxes and find matching predictions
+    match_count = 0
+    pred_match = np.zeros([pred_boxes.shape[0]])
+    gt_match = np.zeros([gt_boxes.shape[0]])
+    for i in range(len(pred_boxes)):
+        # Find best matching ground truth box
+        sorted_ixs = np.argsort(overlaps[i])[::-1]
+        for j in sorted_ixs:
+            # If ground truth box is already matched, go to next one
+            if gt_match[j] == 1:
+                continue
+            # If we reach IoU smaller than the threshold, end the loop
+            iou = overlaps[i, j]
+            if iou < iou_threshold:
+                break
+            # Do we have a match?
+            if pred_class_ids[i] == gt_class_ids[j]:
+                match_count += 1
+                gt_match[j] = 1
+                pred_match[i] = 1
+                break
+
+    # Compute precision and recall at each prediction box step
+    precisions = np.cumsum(pred_match) / (np.arange(len(pred_match)) + 1)
+    recalls = np.cumsum(pred_match).astype(np.float32) / len(gt_match)
+
+    # Pad with start and end values to simplify the math
+    precisions = np.concatenate([[0], precisions, [0]])
+    recalls = np.concatenate([[0], recalls, [1]])
+
+    # Ensure precision values decrease but don't increase. This way, the
+    # precision value at each recall threshold is the maximum it can be
+    # for all following recall thresholds, as specified by the VOC paper.
+    for i in range(len(precisions) - 2, -1, -1):
+        precisions[i] = np.maximum(precisions[i], precisions[i + 1])
+
+    # Compute mean AP over recall range
+    indices = np.where(recalls[:-1] != recalls[1:])[0] + 1
+    mAP = np.sum((recalls[indices] - recalls[indices - 1]) *
+                 precisions[indices])
+
+    return mAP, precisions, recalls, overlaps
